1215b0a653911ac9f3ae48a0cee4b124b60066af
Adrien Boutigny <adrien.boutigny@gmail.com>
2020-07-16 04:07:35 +0200

    prettify everything

--- src/_data/global.js
+++ src/_data/global.js
@@ -1,10 +1,10 @@
 module.exports = {
-	title: 'Morbi tincidunt a nunc eu pellentesque',
-	description:
-		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt illo corrupti itaque atque soluta consectetur, delectus expedita mollitia omnis facilis eum asperiores quod ea pariatur. Inventore quaerat esse blanditiis ullam!',
-	date: '06/07/2021',
-	author: 'Quentin Bellanger',
-	language: 'fr',
-	defaultTheme: 'light',
-	showSlideNumber: true
+  title: 'Morbi tincidunt a nunc eu pellentesque',
+  description:
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt illo corrupti itaque atque soluta consectetur, delectus expedita mollitia omnis facilis eum asperiores quod ea pariatur. Inventore quaerat esse blanditiis ullam!',
+  date: '06/07/2021',
+  author: 'Quentin Bellanger',
+  language: 'fr',
+  defaultTheme: 'light',
+  showSlideNumber: true
 }
